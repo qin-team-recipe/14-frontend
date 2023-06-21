3293b2edf853ae12ad201bb2ba4ff74c6a43321d
ryo28 <wakisan1@ezweb.ne.jp>
2023-06-22 01:14:08 +0900

    ⚡️ 重複しているまたは必要のない記述を削除しました

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,12 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
-    node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-    "prettier",
-  ],
+  extends: ["next/core-web-vitals", "eslint:recommended", "prettier"],
   parserOptions: {
     sourceType: "module",
   },
   rules: {
-    "no-undef": ["error"],
-    "react/prop-types": ["off"],
-    "react/jsx-props-no-spreading": ["off"],
     "no-var": "error",
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
   },
 };
