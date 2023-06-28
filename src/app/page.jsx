f2b03ef3b4d179e3675a67ba61e78a18f9f2ee81
ryo28 <wakisan1@ezweb.ne.jp>
2023-06-28 19:22:44 +0900

    ✨ main部分の中身を作成しました

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,8 @@
+
 export default function Home() {
   return (
-    <>
-      <p className="text-red-500">はろー</p>
-    </>
+    <div>
+      <input className="mx-3  mt-3 w-116 bg-gray-100" type="text" />
+    </div>
   );
 }
