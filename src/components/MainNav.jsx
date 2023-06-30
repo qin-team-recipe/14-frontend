76bd0002bdb6f474501e3f297c50777204e60c87
ryo28 <wakisan1@ezweb.ne.jp>
2023-07-01 00:36:03 +0900

    ✨ ページごとに文字とアイコンの太さが変わるようにしました

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,9 +1,14 @@
-
+"use client";
 import Link from "next/link";
 import { Search } from "tabler-icons-react";
 import { Heart } from "tabler-icons-react";
 import { ShoppingCart } from "tabler-icons-react";
+import { usePathname } from "next/navigation";
+
 export function MainNav() {
+  // pathnameで現在のurlを取得
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     // fixedにしてbottomを効かせて、w-screenでborderを画面横全体に引く＋j-aroundを効かせる
     <div className="fixed sm:sticky left-0 sm:top-0 bottom-0 w-screen sm:w-36 sm:mr-3 z-50">
@@ -16,9 +21,17 @@ export function MainNav() {
               href="/"
               className="flex flex-col sm:flex-row items-center gap-x-2 rounded-3xl sm:p-3 sm:hover:bg-gray-100"
             >
-              <Search size={24} strokeWidth={1} color={"black"} />
+              <Search
+                size={24}
+                strokeWidth={`${pathname === "/" ? 2 : 1}`}
+                color={"black"}
+              />
               {/* ページに合わせて文字の太さを変えたい */}
-              <div className={`font-bold`}>さがす</div>
+              <div
+                className={`${pathname === "/" ? "font-bold" : "font-sans"}`}
+              >
+                さがす
+              </div>
             </Link>
           </li>
           <li>
@@ -26,8 +39,18 @@ export function MainNav() {
               href="/favoriteSearch"
               className="flex flex-col sm:flex-row items-center gap-x-2 rounded-3xl sm:p-3 sm:hover:bg-gray-100"
             >
-              <Heart size={24} strokeWidth={1} color={"black"} />
-              <div>お気に入り</div>
+              <Heart
+                size={24}
+                strokeWidth={`${pathname === "/favoriteSearch" ? 2 : 1}`}
+                color={"black"}
+              />
+              <div
+                className={`${
+                  pathname === "/favoriteSearch" ? "font-bold" : "font-sans"
+                }`}
+              >
+                お気に入り
+              </div>
             </Link>
           </li>
           <li>
@@ -35,8 +58,18 @@ export function MainNav() {
               href="/shoppingList"
               className="flex flex-col sm:flex-row items-center gap-x-2 rounded-3xl sm:p-3 sm:hover:bg-gray-100"
             >
-              <ShoppingCart size={24} strokeWidth={1} color={"black"} />
-              <div>買い物リスト</div>
+              <ShoppingCart
+                size={24}
+                strokeWidth={`${pathname === "/shoppingList" ? 2 : 1}`}
+                color={"black"}
+              />
+              <div
+                className={`${
+                  pathname === "/shoppingList" ? "font-bold" : "font-sans"
+                }`}
+              >
+                買い物リスト
+              </div>
             </Link>
           </li>
         </ul>
