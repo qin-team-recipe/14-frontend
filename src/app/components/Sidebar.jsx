2bb865ec2dd120c73e4c712113662c42c0503a84
takuya <hukujuth@gmail.com>
2023-07-01 09:43:25 +0900

    sidebarの一部修正

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,18 +1,24 @@
+"use client";
+
 import React from "react";
 import SidebarMenu from "./SidebarMenu";
-import { SearchIcon } from "@heroicons/react/solid";
-import { HeartIcon } from "@heroicons/react/solid";
-import { ShoppingBagIcon } from "@heroicons/react/solid";
+import { IconSearch } from "@tabler/icons-react";
 
 const Sidebar = () => {
   return (
     <div className="bg-blue-200 min-h-screen">
       {/* ロゴ */}
       <div className=" ">ロゴ</div>
+      <div className="flex">
+        <IconSearch />
+        <p>探す</p>
+      </div>
+
+
       <div>
-        <SidebarMenu text="探す" Icon={SearchIcon} />
-        <SidebarMenu text="お気に入り" Icon={HeartIcon} />
-        <SidebarMenu text="買い物リスト" Icon={ShoppingBagIcon} />
+        <SidebarMenu text="探す" Icon={IconSearch} />
+        <SidebarMenu text="お気に入り" />
+        <SidebarMenu text="買い物リスト" />
       </div>
     </div>
   );
--- src/app/components/SidebarMenu.jsx
+++ src/app/components/SidebarMenu.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 
-const SidebarMenu = ({ text, Icon }) => {
+const SidebarMenu = ({ text }) => {
   return (
     <div className="flex items-center justify-end">
-      <Icon class="h-7 hidden lg:inline" />
       <span className="hidden lg:inline">{text}</span>
     </div>
   );
