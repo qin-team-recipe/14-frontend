d9245ad69ca13487b5612639924587ee8e4531ce
ryo28 <wakisan1@ezweb.ne.jp>
2023-07-11 02:24:34 +0900

    ⚡️ @tabler/icons-reactに変更し、それぞれ対応させました

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,8 +1,5 @@
 "use client";
 import Link from "next/link";
-import { Search } from "tabler-icons-react";
-import { Heart } from "tabler-icons-react";
-import { ShoppingCart } from "tabler-icons-react";
 import { usePathname } from "next/navigation";
 
 export function MainNav() {
@@ -21,11 +18,23 @@ export function MainNav() {
               href="/"
               className="flex flex-col items-center gap-x-2 rounded-3xl sm:flex-row sm:p-3 sm:hover:bg-gray-100"
             >
-              <Search
-                size={24}
-                strokeWidth={`${pathname === "/" ? 2 : 1}`}
-                color={"black"}
-              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon icon-tabler icon-tabler-search"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width={`${pathname === "/" ? 2 : 1}`}
+                stroke="black"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+                <path d="M21 21l-6 -6"></path>
+              </svg>
+
               {/* 現在ページとurlが等しい時太字にしています */}
               <div
                 className={`${pathname === "/" ? "font-bold" : "font-sans"}`}
@@ -36,17 +45,28 @@ export function MainNav() {
           </li>
           <li>
             <Link
-              href="/favoriteSearch"
+              href="/favorite"
               className="flex flex-col items-center gap-x-2 rounded-3xl sm:flex-row sm:p-3 sm:hover:bg-gray-100"
             >
-              <Heart
-                size={24}
-                strokeWidth={`${pathname === "/favoriteSearch" ? 2 : 1}`}
-                color={"black"}
-              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon icon-tabler icon-tabler-heart"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width={`${pathname === "/favorite" ? 2 : 1}`}
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+              </svg>
+
               <div
                 className={`${
-                  pathname === "/favoriteSearch" ? "font-bold" : "font-sans"
+                  pathname === "/favorite" ? "font-bold" : "font-sans"
                 }`}
               >
                 お気に入り
@@ -55,17 +75,31 @@ export function MainNav() {
           </li>
           <li>
             <Link
-              href="/shoppingList"
+              href="/list"
               className="flex flex-col items-center gap-x-2 rounded-3xl sm:flex-row sm:p-3 sm:hover:bg-gray-100"
             >
-              <ShoppingCart
-                size={24}
-                strokeWidth={`${pathname === "/shoppingList" ? 2 : 1}`}
-                color={"black"}
-              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon icon-tabler icon-tabler-shopping-cart"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width={`${pathname === "/list" ? 2 : 1}`}
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                <path d="M17 17h-11v-14h-2"></path>
+                <path d="M6 5l14 1l-1 7h-13"></path>
+              </svg>
+
               <div
                 className={`${
-                  pathname === "/shoppingList" ? "font-bold" : "font-sans"
+                  pathname === "/list" ? "font-bold" : "font-sans"
                 }`}
               >
                 買い物リスト
