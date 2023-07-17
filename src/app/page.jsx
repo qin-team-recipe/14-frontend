f3f4bfab50ce39bae734a94d5a4353824fd3cf9e
takuya <hukujuth@gmail.com>
2023-07-17 20:13:58 +0900

    修正

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,22 @@
 
 export default function Home() {
   return (
+<<<<<<< Updated upstream
     <div className="flex">
       <input className="w-full border" type="text" />
       <button>Search</button>
+=======
+    <div className="h-screen">
+      <div className="container h-full mx-auto xl:px-50 max-x-5xl">
+        <div className="grid grid-cols-3 h-full">
+          <Sidebar />
+          <div className="text-red-500">
+            
+          </div>
+          <div className="bg-blue-200 min-h-screen"></div>
+        </div>
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 }
