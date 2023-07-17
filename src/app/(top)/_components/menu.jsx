44f7a4eea44e67ca2cd155ae597d7a1e4f9cbe64
shimizu-tomoya <shimitomo.pix@gmail.com>
2023-07-17 09:01:14 +0900

    useStateの追加

--- src/app/(top)/_components/menu.jsx
+++ src/app/(top)/_components/menu.jsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
 import { IconSearch, IconHeart, IconShoppingCart } from "@tabler/icons-react";
+import { useState } from "react";
+
+const [weight, setWeight] = useState[true];
+
+const changeWeight = () => {
+  setWeight((weight) => !weight);
+};
 
 const Navigations = [
   {
@@ -29,7 +36,11 @@ export function Menu() {
         const IconComponent = Navigation.icon;
         return (
           // rounded-fullはborder-radiusのこと。
-          <li key={Navigation.id} className="block flex-1 p-2 visited:bg-gray-200 hover:bg-gray-200 sm:rounded-full">
+          <li
+            key={Navigation.id}
+            className="block flex-1 p-2 hover:bg-gray-200 sm:rounded-full "
+            onClick={changeWeight}
+          >
             <Link href={Navigation.url} className="flex flex-col items-center sm:flex-row">
               <IconComponent />
               <span className="sm:ml-2">{Navigation.text}</span>
