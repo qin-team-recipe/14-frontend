37545022d553b236f6c1e3e46d9c1b2f50c086bf
otaki0413 <takida0413@gmail.com>
2023-08-18 23:46:29 +0900

    🎨 サンプルデータでinput確認

--- src/components/Form/MultiTextField.jsx
+++ src/components/Form/MultiTextField.jsx
@@ -1,6 +1,23 @@
 import { InputAddButton } from "./InputAddButton";
 import { getLabelAndText } from "./utils/helper";
 
+// サンプルデータ あとで消す
+const samples = [
+  {
+    id: 1,
+    value: "https://www.youtube.com/xxx",
+  },
+  {
+    id: 2,
+    value: "https://www.sirogohan.com/xxx",
+  },
+  {
+    id: 3,
+    value:
+      "https://www.test.com/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // 長いやつ
+  },
+];
+
 // フォームで使用するinput行が増えるコンポーネント
 export function MultiTextField({ name, type }) {
   // typeに応じたラベルを取得
@@ -11,25 +28,18 @@ export function MultiTextField({ name, type }) {
       <label htmlFor={name} className="block px-4 text-base font-bold">
         {label}
       </label>
+      {/* inputを複数表示するエリア */}
       <div>
-        <input
-          type="text"
-          id={name}
-          name={name}
-          className="w-full border border-x-0 px-4 py-2 focus:outline-none"
-        />
-        <input
-          type="text"
-          id={name}
-          name={name}
-          className="w-full border border-x-0 px-4 py-2 focus:outline-none"
-        />
-        <input
-          type="text"
-          id={name}
-          name={name}
-          className="w-full border border-x-0 px-4 py-2 focus:outline-none"
-        />
+        {samples.map((val, index) => (
+          <input
+            type="text"
+            key={index}
+            className={`w-full truncate border border-x-0 px-4 py-2 focus:outline-none ${
+              index !== 0 ? "border-t-0" : null
+            }`}
+            value={val.value}
+          />
+        ))}
       </div>
       {/* 〇〇を追加するボタン */}
       <div className="px-4 pt-2">
