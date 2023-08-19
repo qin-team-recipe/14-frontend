03baf66da0e920f534829dfbe7fff9e3696a683c
otaki0413 <takida0413@gmail.com>
2023-08-19 19:11:15 +0900

    ✨ マイレシピ作成画面の用意

--- src/app/(top)/my-recipe/new/page.jsx
+++ src/app/(top)/my-recipe/new/page.jsx
@@ -7,10 +7,15 @@ import {
   TextField,
   getLabelAndText,
 } from "@/components/Form";
+import { IngredientTextField } from "@/components/Form/IngredientTextField";
+import { StepTextField } from "@/components/Form/StepTextField";
 
 // マイレシピ追加画面
 export default function New() {
-  const { label, text } = getLabelAndText("link");
+  // 各typeのラベルと追加ボタンのテキストを取得
+  const ingredient = getLabelAndText("ingredient"); // 材料
+  const step = getLabelAndText("step"); // 作り方
+  const link = getLabelAndText("link"); // リンク
 
   return (
     <div>
@@ -19,13 +24,34 @@ export default function New() {
       </div>
       <div className="mt-4 space-y-8">
         <TextField label="レシピ名" name="name" placeholder="例：肉じゃが" />
+        {/* 材料フィールド */}
+        <IngredientTextField
+          name="ingredient"
+          label={ingredient.label}
+          addButtonElement={<InputAddButton>{ingredient.text}</InputAddButton>}
+          items={INGREDIENTS}
+        />
+        {/* 作り方フィールド */}
+        <StepTextField
+          name="step"
+          label={step.label}
+          addButtonElement={<InputAddButton>{step.text}</InputAddButton>}
+          items={STEPS}
+        />
+        {/* 画像フィールド */}
         <ImageField label="画像（任意）" name="image" />
-        <TextField label="レシピの紹介文（任意）" multiline />
+        {/* レシピの紹介文フィールド */}
+        <TextField
+          label="レシピの紹介文（任意）"
+          name="introduction"
+          multiline
+        />
+        {/* リンクフィールド */}
         <MultiTextField
           name="link"
-          label={label}
-          addButtonElement={<InputAddButton>{text}</InputAddButton>}
-          items={ITEMS}
+          label={link.label}
+          addButtonElement={<InputAddButton>{link.text}</InputAddButton>}
+          items={LINKS}
         />
         <div className="flex items-center justify-center gap-2 px-4">
           <CommonButton bgColor="orange">保存する</CommonButton>
@@ -36,8 +62,44 @@ export default function New() {
   );
 }
 
-// サンプルデータ あとで消す
-const ITEMS = [
+// サンプルデータ（材料）
+const INGREDIENTS = [
+  {
+    id: 1,
+    value: "キャベツ2個",
+  },
+  {
+    id: 2,
+    value: "チーズ",
+  },
+  {
+    id: 3,
+    value:
+      "テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト", // 長いやつ
+  },
+];
+
+// サンプルデータ（作り方）
+const STEPS = [
+  {
+    id: 1,
+    value:
+      "用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。",
+  },
+  {
+    id: 2,
+    value:
+      "用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。",
+  },
+  {
+    id: 3,
+    value:
+      "用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。", // 長いやつ
+  },
+];
+
+// サンプルデータ（リンク）
+const LINKS = [
   {
     id: 1,
     value: "https://www.youtube.com/xxx",
