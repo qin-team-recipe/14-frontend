4e722e889d1b846279a62f857c23a89e9c7e69c5
otaki0413 <takida0413@gmail.com>
2023-08-29 19:04:31 +0900

    🎨 作り方のフィールドはInputタグではないので、pタグで書き直す

--- src/components/Form/StepTextField.jsx
+++ src/components/Form/StepTextField.jsx
@@ -1,45 +1,39 @@
 "use client";
 
+import { IconDotsVertical } from "@tabler/icons-react";
 import clsx from "clsx";
 
-// フォームで使用するinput行が増えるコンポーネント（材料）
+// フォームで使用する作り方のフィールドが増えるコンポーネント（材料）
 export function StepTextField({ name, addButtonElement, label, items }) {
   return (
     <div className="space-y-0.5">
       <label htmlFor={name} className="block px-4 text-base font-bold">
         {label}
       </label>
-      {/* 工程のinputを複数表示するエリア */}
-      <div>
-        {items.map((val, index) => (
-          <div
-            key={val.id}
-            className={clsx(
-              "flex items-center gap-2",
-              "w-full border-0 border-b px-4 py-2",
-              index === 0 && "border-t"
-            )}
-          >
-            <span
+      {/* 作り方のフィールドを複数表示するエリア */}
+      <ul className="divide-y border-y">
+        {items.map((step, index) => (
+          <li key={step.id} className="flex gap-x-2 px-4 py-2">
+            <div
               className={clsx(
-                "h-4 w-4 rounded-full",
-                "flex items-center justify-center",
+                "h-5 w-5 rounded-full",
+                "flex shrink-0 items-center justify-center",
                 "bg-orange-500 text-xs text-white"
               )}
             >
               {index + 1}
-            </span>
-            <input
-              type="text"
-              id={index === 0 ? name : undefined}
-              defaultValue={val.value}
-              placeholder="工程をご記入ください"
-              className="flex-1 text-sm focus:outline-none"
-            />
-          </div>
+            </div>
+            <p className="line-clamp-3 text-sm">{step.value}</p>
+            <div onClick={() => alert("モーダルでます")}>
+              <IconDotsVertical
+                size={20}
+                className="cursor-pointer text-gray-500"
+              />
+            </div>
+          </li>
         ))}
-      </div>
-      {/* 〇〇を追加するボタン */}
+      </ul>
+      {/* 工程を追加するボタン */}
       <div className="px-4 pt-2">{addButtonElement}</div>
     </div>
   );
