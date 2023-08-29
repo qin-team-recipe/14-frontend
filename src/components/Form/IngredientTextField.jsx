f828f9acbcf64af3bdcd709f7f739baa2a2a885b
しまぶー <0120koto@gmail.com>
2023-08-30 00:17:24 +0900

    プルリクレビュー分

--- src/components/Form/IngredientTextField.jsx
+++ src/components/Form/IngredientTextField.jsx
@@ -7,40 +7,40 @@ import clsx from "clsx";
 
 // フォームで使用するinput行が増える材料用コンポーネント
 export function IngredientTextField({ name, addButtonElement, label, items }) {
-  const [num, setNum] = useState(1);
+  const [num, setNum] = useState(2);
 
-  const plus = () => {
+  const increment = () => {
     setNum((prev) => prev + 1);
   };
 
-  const minus = () => {
+  const decrement = () => {
     setNum((prev) => prev - 1);
   };
 
   return (
     <div className="space-y-0.5">
-      <div className="flex items-center gap-x-2 px-4">
-        <label htmlFor={name} className="block text-base font-bold">
+      <div className="flex items-center px-4">
+        <label htmlFor={name} className="block w-24 text-base font-bold">
           {label} / {num}人前
         </label>
         {/* 材料の◯人前を増減させるボタン */}
-        <div className="flex gap-1.5">
+        <div className="flex gap-x-1.5">
           <button
-            onClick={minus}
+            onClick={decrement}
             disabled={num === 1}
             className={clsx(
               "cursor-pointer bg-orange-50 text-orange-700",
-              num === 1 && "cursor-not-allowed bg-gray-100 text-gray-300"
+              "disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-300"
             )}
           >
             <IconMinus size={16} />
           </button>
           <button
-            onClick={plus}
+            onClick={increment}
             disabled={num === 6}
             className={clsx(
               "cursor-pointer bg-orange-50 text-orange-700",
-              num === 6 && "cursor-not-allowed bg-gray-100 text-gray-300"
+              "disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-300"
             )}
           >
             <IconPlus size={16} />
@@ -48,16 +48,11 @@ export function IngredientTextField({ name, addButtonElement, label, items }) {
         </div>
       </div>
       {/* 材料のinputを複数表示するエリア */}
-      <div>
+      <ul className="divide-y border-y">
         {items.map((val, index) => (
-          <div
+          <li
             key={val.id}
-            className={clsx(
-              "flex items-center justify-between gap-4",
-              "px-4 py-2",
-              "border-0 border-b",
-              index === 0 && "border-t"
-            )}
+            className="flex items-center justify-between gap-x-4 px-4 py-2"
           >
             <input
               type="text"
@@ -65,15 +60,16 @@ export function IngredientTextField({ name, addButtonElement, label, items }) {
               defaultValue={val.value}
               className="flex-1 text-sm focus:outline-none"
             />
-            <div onClick={() => alert("モーダルでます")}>
+            <button onClick={() => alert("TODO: モーダルでます")}>
               <IconDotsVertical
                 size={20}
-                className="cursor-pointer text-gray-500"
+                stroke={1.5}
+                className="text-gray-500"
               />
-            </div>
-          </div>
+            </button>
+          </li>
         ))}
-      </div>
+      </ul>
       {/* 材料を追加するボタン */}
       <div className="px-4 pt-2">{addButtonElement}</div>
     </div>
--- src/components/Form/StepTextField.jsx
+++ src/components/Form/StepTextField.jsx
@@ -13,7 +13,7 @@ export function StepTextField({ name, addButtonElement, label, items }) {
       {/* 作り方のフィールドを複数表示するエリア */}
       <ul className="divide-y border-y">
         {items.map((step, index) => (
-          <li key={step.id} className="flex gap-x-2 px-4 py-2">
+          <li key={step.id} className="flex items-start gap-x-2 px-4 py-2">
             <div
               className={clsx(
                 "h-5 w-5 rounded-full",
@@ -23,13 +23,16 @@ export function StepTextField({ name, addButtonElement, label, items }) {
             >
               {index + 1}
             </div>
-            <p className="line-clamp-3 text-sm">{step.value}</p>
-            <div onClick={() => alert("モーダルでます")}>
+            <p className="line-clamp-3 flex-1 break-all text-sm">
+              {step.value}
+            </p>
+            <button onClick={() => alert("モーダルでます")}>
               <IconDotsVertical
                 size={20}
+                stroke={1.5}
                 className="cursor-pointer text-gray-500"
               />
-            </div>
+            </button>
           </li>
         ))}
       </ul>
