4e9c646007c456f9b8f1bfab1be238ee49456728
しまぶー <0120koto@gmail.com>
2023-09-06 23:24:25 +0900

    プルリクレビュー分

--- src/components/ShowMore/ShowMore.jsx
+++ src/components/ShowMore/ShowMore.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function ShowMore({ href }) {
   return (
-    <Link className="font-bold text-zinc-400 hover:underline" href={href}>
+    <Link className="font-bold text-gray-400 hover:underline" href={href}>
       もっと見る
     </Link>
   );
