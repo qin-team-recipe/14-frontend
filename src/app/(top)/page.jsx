4bbd90e8dc70274f1b8febd8acfda178bad45890
しまぶー <0120koto@gmail.com>
2023-09-06 23:57:18 +0900

    プルリクレビュー分

--- src/app/(top)/page.jsx
+++ src/app/(top)/page.jsx
@@ -1,9 +1,9 @@
-import { SearchBox } from "@/components/SearchBox/SearchBox";
+import { SearchBox } from "@/components/SearchBox";
 
 export default function Home() {
   return (
-    <>
+    <div className="px-4 py-2">
       <SearchBox />
-    </>
+    </div>
   );
 }
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -2,15 +2,13 @@ import { IconSearch } from "@tabler/icons-react";
 
 export function SearchBox() {
   return (
-    <div className="px-4 py-2">
-      <div className="flex w-full items-center justify-center rounded-md bg-gray-200 px-2">
-        <IconSearch className="text-gray-500" />
-        <input
-          type="text"
-          placeholder="シェフやレシピを検索"
-          className="w-full rounded-md bg-gray-200 py-2 pl-2 font-bold outline-none placeholder:text-gray-500 hover:outline-none"
-        />
-      </div>
+    <div className="flex w-full items-center gap-x-2 rounded-lg bg-gray-100 px-3">
+      <IconSearch className="h-6 w-6 text-gray-500" />
+      <input
+        type="text"
+        placeholder="シェフやレシピを検索"
+        className="w-full bg-transparent py-2 font-bold outline-none placeholder:text-gray-500"
+      />
     </div>
   );
 }
