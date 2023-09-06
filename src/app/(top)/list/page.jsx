4611e42936ac9f9afb9653e7e264c57755b1570a
takuya <hukujuth@gmail.com>
2023-09-06 15:59:41 +0900

    shoppingListフォルダの作成

--- src/app/(top)/list/page.jsx
+++ src/app/(top)/list/page.jsx
@@ -1,12 +1,15 @@
 import { AuthScreen } from "@/components/Auth";
 import { ListBar } from "@/components/Bar";
 import src from "~/images/cookingGirl.png";
+import { ShoppingList } from "../shoppinsList/ShoppingList";
 
 export default function List() {
+  const isSignedIn = true;
+
   return (
     <div className="px-4 py-3">
       <ListBar />
-      <AuthScreen src={src} />
+      {isSignedIn ? <ShoppingList /> : <AuthScreen src={src} />}
     </div>
   );
 }
---
+++ src/app/(top)/shoppinsList/ShoppingList.jsx
@@ -0,0 +1,3 @@
+export function ShoppingList() {
+  return <div>Helloaaa</div>
+}
