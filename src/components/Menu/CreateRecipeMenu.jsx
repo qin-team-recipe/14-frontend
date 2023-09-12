375976438086a631fe45159956bfc71e526ff84e
otaki0413 <takida0413@gmail.com>
2023-09-12 22:36:37 +0900

    🎨 コンポーネント名変更

--- src/components/Menu/CreateRecipeMenu.jsx
+++ src/components/Menu/CreateRecipeMenu.jsx
@@ -4,7 +4,7 @@ import {
   IconEdit,
   IconTrash,
 } from "@tabler/icons-react";
-import { MenuContainer } from "./MenuContainer";
+import { MenuItemContainer } from "./MenuItemContainer";
 
 // マイレシピ作成画面で使用するメニュー
 export function CreateRecipeMenu() {
@@ -12,12 +12,18 @@ export function CreateRecipeMenu() {
     <div className="w-64 rounded-md border shadow-xl">
       <div className="divide-y text-gray-500">
         <div className="space-y-3 px-3 py-2.5 text-gray-500">
-          <MenuContainer icon={IconEdit}>編集する</MenuContainer>
-          <MenuContainer icon={IconChevronUp}>上に移動する</MenuContainer>
-          <MenuContainer icon={IconChevronDown}>下に移動する</MenuContainer>
+          <MenuItemContainer icon={IconEdit}>編集する</MenuItemContainer>
+          <MenuItemContainer icon={IconChevronUp}>
+            上に移動する
+          </MenuItemContainer>
+          <MenuItemContainer icon={IconChevronDown}>
+            下に移動する
+          </MenuItemContainer>
         </div>
         <div className="space-y-3 px-3 py-2.5 text-gray-500">
-          <MenuContainer icon={IconTrash}>リストから削除する</MenuContainer>
+          <MenuItemContainer icon={IconTrash}>
+            リストから削除する
+          </MenuItemContainer>
         </div>
       </div>
     </div>
--- src/components/Menu/MenuItemContainer.jsx
+++ src/components/Menu/MenuItemContainer.jsx
@@ -1,4 +1,5 @@
-export function MenuContainer({ children, icon: Icon }) {
+// メニュー内の単一アイテムを囲うコンポーネント
+export function MenuItemContainer({ children, icon: Icon }) {
   return (
     <div className="flex items-center gap-x-2">
       <Icon size={16} stroke={1.5} />
