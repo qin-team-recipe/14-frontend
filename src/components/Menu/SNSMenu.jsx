fdad21a9a691c698f8890ae9a6c063e6e4b7eb85
otaki0413 <takida0413@gmail.com>
2023-09-16 18:59:49 +0900

    ✨ itemsに表示したいアイテムを入れる形式に変更

--- src/components/Menu/SNSMenu.jsx
+++ src/components/Menu/SNSMenu.jsx
@@ -1,39 +1,68 @@
+import Link from "next/link";
 import {
   IconBrandFacebook,
   IconBrandTiktok,
   IconBrandTwitter,
   IconLink,
 } from "@tabler/icons-react";
+import { MenuItemContainer } from "./MenuItemContainer";
+import { MenuTest } from "./MenuTest";
 
+// TODO: Linkのhrefに正しいリンクを指定する
 export function SNSMenu() {
   return (
-    <div className="w-64 rounded-md border shadow-xl">
-      <div className="divide-y text-gray-500">
-        <div className="space-y-3 px-3 py-2.5 text-gray-500">
-          <div className="flex items-center gap-x-2">
-            <IconBrandTiktok size={16} stroke={1.5} />
-            <div className="text-sm">TikTok</div>
-          </div>
-          <div className="flex items-center gap-x-2">
-            <IconBrandTwitter size={16} stroke={1.5} />
-            <div className="text-sm">Twitter</div>
-          </div>
-          <div className="flex items-center gap-x-2">
-            <IconBrandFacebook size={16} stroke={1.5} />
-            <div className="text-sm">Facebook</div>
-          </div>
-        </div>
-        <div className="space-y-3 px-3 py-2.5 text-gray-500">
-          <div className="flex items-center gap-x-2">
-            <IconLink size={16} stroke={1.5} />
-            <div className="text-sm">hogehoge.com</div>
-          </div>
-          <div className="flex items-center gap-x-2">
-            <IconLink size={16} stroke={1.5} />
-            <div className="text-sm">foobar.com</div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <MenuTest
+      items={[
+        {
+          type: "item",
+          key: "tiktok",
+          component: (
+            <MenuItemContainer icon={IconBrandTiktok}>
+              <Link href="#">TikTok</Link>
+            </MenuItemContainer>
+          ),
+        },
+        {
+          type: "item",
+          key: "twitter",
+          component: (
+            <MenuItemContainer icon={IconBrandTwitter}>
+              <Link href="#">Twitter</Link>
+            </MenuItemContainer>
+          ),
+        },
+        {
+          type: "item",
+          key: "facebook",
+          component: (
+            <MenuItemContainer icon={IconBrandFacebook}>
+              <Link href="#">Facebook</Link>
+            </MenuItemContainer>
+          ),
+        },
+        {
+          type: "divider",
+          key: "divider1",
+        },
+        {
+          type: "item",
+          key: "hogehoge",
+          component: (
+            <MenuItemContainer icon={IconLink}>
+              <Link href="#">hogehoge.com</Link>
+            </MenuItemContainer>
+          ),
+        },
+        {
+          type: "item",
+          key: "foobar",
+          component: (
+            <MenuItemContainer icon={IconLink}>
+              <Link href="#">foobar.com</Link>
+            </MenuItemContainer>
+          ),
+        },
+      ]}
+    />
   );
 }
