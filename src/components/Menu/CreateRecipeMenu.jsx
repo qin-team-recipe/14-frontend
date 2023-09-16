575d8aa21b0721b33be2300bf73a6b895c5673ac
otaki0413 <takida0413@gmail.com>
2023-09-16 19:47:43 +0900

    🎨 マイレシピ追加画面のメニューの構造を修正

--- src/components/Menu/CreateRecipeMenu.jsx
+++ src/components/Menu/CreateRecipeMenu.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   IconChevronDown,
   IconChevronUp,
@@ -5,27 +7,92 @@ import {
   IconTrash,
 } from "@tabler/icons-react";
 import { MenuItemContainer } from "./MenuItemContainer";
+import { MenuTest } from "./MenuTest";
 
 // マイレシピ作成画面で使用するメニュー
-export function CreateRecipeMenu() {
-  return (
-    <div className="w-64 rounded-md border shadow-xl">
-      <div className="divide-y text-gray-500">
-        <div className="space-y-3 px-3 py-2.5 text-gray-500">
-          <MenuItemContainer icon={IconEdit}>編集する</MenuItemContainer>
-          <MenuItemContainer icon={IconChevronUp}>
-            上に移動する
-          </MenuItemContainer>
-          <MenuItemContainer icon={IconChevronDown}>
-            下に移動する
-          </MenuItemContainer>
-        </div>
-        <div className="space-y-3 px-3 py-2.5 text-gray-500">
-          <MenuItemContainer icon={IconTrash}>
-            リストから削除する
-          </MenuItemContainer>
-        </div>
-      </div>
-    </div>
+// TODO: buttonのonClickに処理を追加
+export function CreateRecipeMenu({ isEdit }) {
+  // 編集用のメニューかどうかで切り替える
+  return isEdit ? (
+    <MenuTest
+      items={[
+        {
+          type: "item",
+          key: "edit",
+          component: (
+            <MenuItemContainer icon={IconEdit}>
+              <button onClick={() => {}}>編集する</button>
+            </MenuItemContainer>
+          ),
+        },
+        {
+          type: "item",
+          key: "up",
+          component: (
+            <MenuItemContainer icon={IconChevronUp}>
+              <button onClick={() => {}}>上に移動する</button>
+            </MenuItemContainer>
+          ),
+        },
+        {
+          type: "item",
+          key: "down",
+          component: (
+            <MenuItemContainer icon={IconChevronDown}>
+              <button onClick={() => {}}>下に移動する</button>
+            </MenuItemContainer>
+          ),
+        },
+        {
+          type: "divider",
+          key: "divider1",
+        },
+        {
+          type: "item",
+          key: "delete",
+          component: (
+            <MenuItemContainer icon={IconTrash}>
+              <button onClick={() => {}}>リストから削除する</button>
+            </MenuItemContainer>
+          ),
+        },
+      ]}
+    />
+  ) : (
+    <MenuTest
+      items={[
+        {
+          type: "item",
+          key: "up",
+          component: (
+            <MenuItemContainer icon={IconChevronUp}>
+              <button onClick={() => {}}>上に移動する</button>
+            </MenuItemContainer>
+          ),
+        },
+        {
+          type: "item",
+          key: "down",
+          component: (
+            <MenuItemContainer icon={IconChevronDown}>
+              <button onClick={() => {}}>下に移動する</button>
+            </MenuItemContainer>
+          ),
+        },
+        {
+          type: "divider",
+          key: "divider1",
+        },
+        {
+          type: "item",
+          key: "delete",
+          component: (
+            <MenuItemContainer icon={IconTrash}>
+              <button onClick={() => {}}>リストから削除する</button>
+            </MenuItemContainer>
+          ),
+        },
+      ]}
+    />
   );
 }
