79d71fee8b416d2e2a6ed79cec83a615f5be3fbb
ryo28 <wakisan1@ezweb.ne.jp>
2023-09-18 07:21:40 +0900

    ✨ もっと見るリンクから飛べる、chefページとrecipeページを作成しました

---
+++ src/app/(top)/search/recipe/page.jsx
@@ -0,0 +1,16 @@
+import { SearchBox } from "@/components/SearchBox";
+import { Tabs } from "@/components/Tabs";
+
+export default function Recipe() {
+  return (
+    <>
+     <div className="p-4"><SearchBox /></div>
+      <Tabs
+        tabItems={[
+          { label: "レシピ", href: "/search/recipe" },
+          { label: "シェフ", href: "/search/chef" },
+        ]}
+      />
+    </>
+  );
+}
