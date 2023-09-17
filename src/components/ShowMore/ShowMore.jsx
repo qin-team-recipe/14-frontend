32fa04757087c7d912e1bde72b1309105aa7d14e
ryo28 <wakisan1@ezweb.ne.jp>
2023-09-18 06:24:57 +0900

    ✨ もっと見るボタンにテキストを追加できるようにしました、テキストにフォントを当てました

--- src/components/ShowMore/ShowMore.jsx
+++ src/components/ShowMore/ShowMore.jsx
@@ -1,9 +1,21 @@
 import Link from "next/link";
+import { Zen_Antique } from "@next/font/google";
+import clsx from "clsx";
 
-export function ShowMore({ href }) {
+const ZenAntique = Zen_Antique({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export function ShowMore({ href, text }) {
   return (
-    <Link className="font-bold text-gray-400 hover:underline" href={href}>
-      もっと見る
-    </Link>
+    <>
+      <div className="flex items-center justify-between ">
+        <p className={clsx("text-xl", ZenAntique.className)}>{`${text === undefined ? "":text}`}</p>
+        <Link className="font-bold text-gray-400 hover:underline" href={href}>
+          もっと見る
+        </Link>
+      </div>
+    </>
   );
 }
