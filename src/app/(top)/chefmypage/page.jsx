1c150410505a526d43ed53937a718db67af68d08
takuya <hukujuth@gmail.com>
2023-09-18 20:50:36 +0900

    ファイルの作成

---
+++ src/app/(top)/chefmypage/page.jsx
@@ -0,0 +1,13 @@
+import { IconDotsCircleHorizontal } from "@tabler/icons-react";
+import { IconArrowNarrowLeft } from "@tabler/icons-react";
+
+export default function Profile() {
+  return (
+    <div className="px-4">
+      <div className="flex justify-between">
+        <IconArrowNarrowLeft />
+        <IconDotsCircleHorizontal />
+      </div>
+    </div>
+  );
+}
