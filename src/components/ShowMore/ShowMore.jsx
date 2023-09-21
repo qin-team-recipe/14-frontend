116b20d8c63b19c40de9159578f28093986ffbdd
しまぶー <0120koto@gmail.com>
2023-09-22 01:32:06 +0900

    fix @next/fontを削除し、ビルトインのnext/fontを使うように変更

--- src/components/ShowMore/ShowMore.jsx
+++ src/components/ShowMore/ShowMore.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Zen_Antique } from "@next/font/google";
+import { Zen_Antique } from "next/font/google";
 import clsx from "clsx";
 
 const ZenAntique = Zen_Antique({
