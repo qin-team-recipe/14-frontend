7e6136a70aef498b16725c45ad5992f6e6e42313
shimizu-tomoya <shimitomo.pix@gmail.com>
2023-09-26 00:42:59 +0900

    [udpate]画像の取り込み

--- src/app/(top)/layout.jsx
+++ src/app/(top)/layout.jsx
@@ -1,9 +1,10 @@
 import { Navigation } from "../_components/Navigation";
 import { Tabs } from "@/components/Tabs";
 
-export default function TopLayout({ children }) {
+export default function TopLayout({ children, params }) {
+  const paramsId = params.id;
   return (
-    <div className="mx-auto flex min-h-screen sm:max-w-2xl sm:gap-x-5 sm:px-4">
+    <div className="mx-auto flex sm:max-w-2xl sm:gap-x-5 sm:px-4">
       <Navigation />
       <main className="w-full overflow-hidden border-gray-200 pb-5 sm:border-x">
         {children}
@@ -11,11 +12,11 @@ export default function TopLayout({ children }) {
           tabItems={[
             {
               label: "新着レシピ",
-              href: "/profile/[slug]/",
+              href: `/profile/${paramsId}`,
             },
             {
               label: "人気レシピ",
-              href: "/profile/[slug]/newrecipe",
+              href: `/profile/${paramsId}/newrecipe`,
             },
           ]}
         />
