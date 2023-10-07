29bd8248df05871365a807b112de3605f8c718c7
otaki0413 <takida0413@gmail.com>
2023-10-07 17:48:17 +0900

    🎨 ダミーデータの構造修正とFavCountBadgeコンポーネント追加

--- src/components/RecipeList/HorizontalRecipeList.jsx
+++ src/components/RecipeList/HorizontalRecipeList.jsx
@@ -3,14 +3,17 @@
 import Link from "next/link";
 import Image from "next/image";
 import useEmblaCarousel from "embla-carousel-react";
+import { FavCountBadge } from "../Badge";
 
-const DUMMY_DATE = [
+const DUMMY_DATA = [
   {
     id: 1,
     recipeName:
       "トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン",
     chefName: "ウルトラハイパー超すごいしまぶーシェフ",
     image: "https://picsum.photos/160/160?random-1",
+    favCount: 1234,
+    isPublished: true,
   },
   {
     id: 2,
@@ -19,6 +22,8 @@ const DUMMY_DATE = [
     chefName: "ウルトラハイパー超すごいしまぶーシェフ",
 
     image: "https://picsum.photos/160/160?random-2",
+    favCount: 1234,
+    isPublished: false,
   },
   {
     id: 3,
@@ -26,6 +31,8 @@ const DUMMY_DATE = [
       "トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン",
     chefName: "ウルトラハイパー超すごいしまぶーシェフ",
     image: "https://picsum.photos/160/160?random-3",
+    favCount: 123400000000,
+    isPublished: true,
   },
   {
     id: 4,
@@ -33,6 +40,8 @@ const DUMMY_DATE = [
       "トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン",
     chefName: "ウルトラハイパー超すごいしまぶーシェフ",
     image: "https://picsum.photos/160/160?random-4",
+    favCount: 0,
+    isPublished: false,
   },
   {
     id: 5,
@@ -41,6 +50,8 @@ const DUMMY_DATE = [
     chefName: "ウルトラハイパー超すごいしまぶーシェフ",
 
     image: "https://picsum.photos/160/160?random-5",
+    favCount: 1234,
+    isPublished: true,
   },
   {
     id: 6,
@@ -49,6 +60,8 @@ const DUMMY_DATE = [
     chefName: "ウルトラハイパー超すごいしまぶーシェフ",
 
     image: "https://picsum.photos/160/160?random-6",
+    favCount: 0,
+    isPublished: true,
   },
   {
     id: 7,
@@ -57,6 +70,8 @@ const DUMMY_DATE = [
     chefName: "ウルトラハイパー超すごいしまぶーシェフ",
 
     image: "https://picsum.photos/160/160?random-7",
+    favCount: 1234,
+    isPublished: true,
   },
 ];
 
@@ -66,25 +81,28 @@ export function HorizontalRecipeList() {
   return (
     <div ref={emblaRef}>
       <div className="flex gap-4">
-        {DUMMY_DATE.map((chef) => (
+        {DUMMY_DATA.map((recipe) => (
           <Link
             className="relative w-40 flex-shrink-0"
-            key={chef.id}
+            key={recipe.id}
             href="#TODO"
           >
             <Image
               className="rounded-2xl"
-              key={chef.id}
-              src={chef.image}
-              alt={chef.recipeName}
+              key={recipe.id}
+              src={recipe.image}
+              alt={recipe.recipeName}
               width={160}
               height={160}
             />
+            <FavCountBadge isPublished={recipe.isPublished}>
+              {recipe.favCount.toLocaleString()}
+            </FavCountBadge>
             <div className="mt-2 line-clamp-2 text-sm font-bold">
-              {chef.recipeName}
+              {recipe.recipeName}
             </div>
             <div className="mt-1 line-clamp-1 text-xs text-gray-500">
-              {chef.chefName}
+              {recipe.chefName}
             </div>
           </Link>
         ))}
--- src/components/RecipeList/VerticalRecipeList.jsx
+++ src/components/RecipeList/VerticalRecipeList.jsx
@@ -2,14 +2,17 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { FavCountBadge } from "../Badge";
 
-const DUMMY_DATE = [
+const DUMMY_DATA = [
   {
     id: 1,
     recipeName:
       "トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン",
     chefName: "ウルトラハイパー超すごいしまぶーシェフ",
     image: "https://picsum.photos/160/160?random-1",
+    favCount: 1234,
+    isPublished: true,
   },
   {
     id: 2,
@@ -18,6 +21,8 @@ const DUMMY_DATE = [
     chefName: "マイシェフ",
 
     image: "https://picsum.photos/160/160?random-2",
+    favCount: 1234,
+    isPublished: false,
   },
   {
     id: 3,
@@ -25,6 +30,8 @@ const DUMMY_DATE = [
       "トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン",
     chefName: "ウルトラハイパー超すごいしまぶーシェフ",
     image: "https://picsum.photos/160/160?random-3",
+    favCount: 1234,
+    isPublished: true,
   },
   {
     id: 4,
@@ -32,6 +39,8 @@ const DUMMY_DATE = [
       "トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン",
     chefName: "ウルトラハイパー超すごいしまぶーシェフ",
     image: "https://picsum.photos/160/160?random-4",
+    favCount: 456,
+    isPublished: true,
   },
   {
     id: 5,
@@ -40,6 +49,8 @@ const DUMMY_DATE = [
     chefName: "ウルトラハイパー超すごいしまぶーシェフ",
 
     image: "https://picsum.photos/160/160?random-5",
+    favCount: 50000,
+    isPublished: false,
   },
   {
     id: 6,
@@ -48,6 +59,8 @@ const DUMMY_DATE = [
     chefName: "ウルトラハイパー超すごいしまぶーシェフ",
 
     image: "https://picsum.photos/160/160?random-6",
+    favCount: 50000,
+    isPublished: true,
   },
   {
     id: 7,
@@ -56,14 +69,16 @@ const DUMMY_DATE = [
     chefName: "ウルトラハイパー超すごいしまぶーシェフ",
 
     image: "https://picsum.photos/160/160?random-7",
+    favCount: 1234,
+    isPublished: true,
   },
 ];
 
 export function VerticalRecipeList() {
   return (
     <div className="grid grid-cols-2 gap-4">
-      {DUMMY_DATE.map((recipe) => (
-        <Link key={recipe.id} href="#TODO">
+      {DUMMY_DATA.map((recipe) => (
+        <Link key={recipe.id} href="#TODO" className="relative">
           <Image
             className="w-full rounded-2xl"
             key={recipe.id}
@@ -72,6 +87,9 @@ export function VerticalRecipeList() {
             width={200}
             height={200}
           />
+          <FavCountBadge isPublished={recipe.isPublished}>
+            {recipe.favCount.toLocaleString()}
+          </FavCountBadge>
 
           <div className="mt-2 line-clamp-2 text-sm font-bold">
             {recipe.recipeName}
