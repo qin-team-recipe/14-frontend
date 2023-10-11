9cd465dfcf4fb57cbe3e56cff7708714ebb14d46
otaki0413 <takida0413@gmail.com>
2023-10-11 23:17:32 +0900

    ✨ プロフィール編集ボタンの作成

---
+++ src/app/(top)/my-page/(_)/_components/ProfileEditButton.jsx
@@ -0,0 +1,7 @@
+export function ProfileEditButton({ children }) {
+  return (
+    <button className="block w-full rounded border border-black px-3 py-1 text-center text-sm">
+      {children}
+    </button>
+  );
+}
---
+++ src/app/(top)/my-page/(_)/_components/index.js
@@ -0,0 +1,1 @@
+export { ProfileEditButton } from "./ProfileEditButton";
