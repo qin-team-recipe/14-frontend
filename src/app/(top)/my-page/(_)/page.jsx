d7c7da092244a3fcf6f078af3c7c379a9e31bcb2
otaki0413 <takida0413@gmail.com>
2023-10-12 00:56:44 +0900

    ✨ マイページのデフォルト画面(新着レシピ)と人気レシピのページ作成

---
+++ src/app/(top)/my-page/(_)/page.jsx
@@ -0,0 +1,9 @@
+import { VerticalRecipeList } from "@/components/RecipeList";
+
+export default function myPage() {
+  return (
+    <div className="px-4 pt-5">
+      <VerticalRecipeList />
+    </div>
+  );
+}
---
+++ src/app/(top)/my-page/(_)/popular/page.jsx
@@ -0,0 +1,9 @@
+import { VerticalRecipeList } from "@/components/RecipeList";
+
+export default function Popular() {
+  return (
+    <div className="px-4 pt-5">
+      <VerticalRecipeList />
+    </div>
+  );
+}
