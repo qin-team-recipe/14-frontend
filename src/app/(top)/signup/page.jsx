706f4744bf48509d6d1f6637d5eeb29e5c92ab5c
otaki0413 <takida0413@gmail.com>
2023-10-14 18:04:32 +0900

    ✨ 新規登録ページの作成

---
+++ src/app/(top)/signup/page.jsx
@@ -0,0 +1,23 @@
+import { SignUpBar } from "@/components/Bar";
+import { CommonButton, TextField } from "@/components/Form";
+
+export default function SignUp() {
+  return (
+    <div>
+      <div className="px-4 py-3">
+        <SignUpBar />
+      </div>
+      <div className="mb-12 mt-5 space-y-8">
+        <TextField
+          label="ニックネーム"
+          name="name"
+          placeholder="ニックネームをご入力ください"
+        />
+        <div className="flex items-center justify-center gap-4 px-4">
+          <CommonButton bgColor="orange">登録する</CommonButton>
+          <CommonButton>ログアウト</CommonButton>
+        </div>
+      </div>
+    </div>
+  );
+}
