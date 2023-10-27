10409f4ec316a8b3a07f5fc010685d18af98ee7c
ryo28 <wakisan1@ezweb.ne.jp>
2023-10-27 21:11:53 +0900

    ♻️ 自動整形させました

--- src/components/Sns/Sns.jsx
+++ src/components/Sns/Sns.jsx
@@ -1,23 +1,22 @@
 import {
-    IconBrandYoutube,
-    IconBrandInstagram,
-    IconDotsCircleHorizontal,
-  } from "@tabler/icons-react";
-  import Link from "next/link";
-  
-  export function Sns() {
-    return (
-      <div className="flex gap-3">
-        <Link href="https://www.youtube.com/" target="_blank">
-          <IconBrandYoutube stroke={1.5} />
-        </Link>
-        <Link href="https://www.instagram.com/" target="_blank">
-          <IconBrandInstagram stroke={1.5} />
-        </Link>
-        <button>
-          <IconDotsCircleHorizontal stroke={1.5} />
-        </button>
-      </div>
-    );
-  }
-  
+  IconBrandYoutube,
+  IconBrandInstagram,
+  IconDotsCircleHorizontal,
+} from "@tabler/icons-react";
+import Link from "next/link";
+
+export function Sns() {
+  return (
+    <div className="flex gap-3">
+      <Link href="https://www.youtube.com/" target="_blank">
+        <IconBrandYoutube stroke={1.5} />
+      </Link>
+      <Link href="https://www.instagram.com/" target="_blank">
+        <IconBrandInstagram stroke={1.5} />
+      </Link>
+      <button>
+        <IconDotsCircleHorizontal stroke={1.5} />
+      </button>
+    </div>
+  );
+}
