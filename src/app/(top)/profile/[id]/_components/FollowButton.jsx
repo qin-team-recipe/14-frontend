d6b178bacc837dc859677463008120c4e3b628eb
otaki0413 <takida0413@gmail.com>
2023-11-05 21:15:33 +0900

    🎨 フォローするボタン修正(PR取込後)

--- src/app/(top)/profile/[id]/_components/FollowButton.jsx
+++ src/app/(top)/profile/[id]/_components/FollowButton.jsx
@@ -1,11 +1,23 @@
-export function FollowButton({ children }) {
+"use client";
+import clsx from "clsx";
+import { useState } from "react";
+
+export function FollowButton() {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const addFollow = () => {
+    setIsFollowing((prev) => !prev);
+  };
+
   return (
     <button
-      className={`
-        block w-full rounded bg-[#E54D2E] px-3 py-1
-        text-center text-sm text-white`}
+      className={clsx(
+        isFollowing ? "bg-white text-[#E54D2E]" : "bg-[#E54D2E] text-white",
+        "block w-full rounded border border-[#E54D2E] px-3 py-1 text-center text-sm",
+      )}
+      onClick={addFollow}
     >
-      {children}
+      {isFollowing ? "フォロー中" : "フォローする"}
     </button>
   );
 }
