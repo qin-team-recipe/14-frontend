6f2d2c4465226dbd310bcabc7822859445b3e098
ryo28 <wakisan1@ezweb.ne.jp>
2023-11-21 06:04:20 +0900

    ♻️ 自動整形させました

--- src/app/(top)/recipe/[id]/_components/Dropdown.jsx
+++ src/app/(top)/recipe/[id]/_components/Dropdown.jsx
@@ -1,12 +1,7 @@
 import { useDetectClickOutside } from "react-detect-click-outside";
 
 // 渡したコンポーネント以外をクリックすると発火
-export function Dropdown({ closeToggle,components }){
+export function Dropdown({ closeToggle, components }) {
   const ref = useDetectClickOutside({ onTriggered: closeToggle });
-  return (
-    <div ref={ref}>
-     {components}
-    </div>
-  );
-};
-
+  return <div ref={ref}>{components}</div>;
+}
